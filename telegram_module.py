9b442b3ef1310996062ebd8af5e8d41e806b62f4
C4NU <canu1832@gmail.com>
2021-09-21 01:27:28 +0900

    [ADD] / 명령어 를 통한 봇 작동 함수 2가지 작성, Handler 함수 작성.

--- telegram_module.py
+++ telegram_module.py
@@ -1,15 +1,39 @@
 import telegram
-from telegram.ext import Updater
+from telegram.ext import Updater, dispatcher
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
+
 class TelegramBot:
 	# 클래스 생성자
-	def __init__(self, token, chatID):
-		self.bot = telegram.Bot(token=token)
-		self.chatID = chatID
-		self.updates = self.bot.getUpdates()
-	# 메시지 전송 함수
+	def __init__(self, _token, _chatID):
+		self.bot = telegram.Bot(token=_token)
+		self.chatID = _chatID
+		self.updater = Updater(token=_token, use_context=True)
+		self.dispatcher = self.updater.dispatcher
+    # 메시지 전송 함수
 	def SendTelegramMessage(self, _description):
 		self.bot.sendMessage(chat_id=self.chatID, text=_description)
-    # 메시지 
+    # 봇 시작 함수
+	def StartBot(self, update, context):
+		self.SendTelegramMessage("봇 작동 시작.")
+    # 봇 정지 함수
+	def StopBot(self, update, context):
+		self.SendTelegramMessage("봇 작동 종료.")
+    # CommandHandler 생성 함수
+	def HandlerInitialize(self):
+		self.startHandler = CommandHandler('start', self.StartBot)
+		self.stopHandler = CommandHandler('stop', self.StopBot)
+		self.dispatcher.add_handler(self.startHandler)
+		self.dispatcher.add_handler(self.stopHandler)
+
+# 클래스 테스트 용 메인 함수
+def main():
+    Bot=TelegramBot('2039263005:AAF_KVYVpcBHvBEbRPvlwtsj_njNRtpEE0E',305295334)
+    Bot.HandlerInitialize()
+
+    Bot.updater.start_polling()
+    Bot.updater.idle()
+    
+if __name__ == '__main__':
+    main()
