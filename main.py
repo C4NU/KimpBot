86925b6ecae6ee43bea5e0098a66d3cc980dbe49
C4NU <canu1832@gmail.com>
2021-09-22 21:51:35 +0900

    [FIXED] 불필요한 package 정리, 코드 캡슐화 완료.

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
-from typing import Text
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from os import path
-from requests_html import HTMLSession
-import time
-
 import telegram_module as MessageModule
-import premium_module as Premium
+import premium_module as PremiumModule
+
 # 메인 함수
 def main():
     # initialize class
     bot = MessageModule.TelegramBot(
     	'2039263005:AAF_KVYVpcBHvBEbRPvlwtsj_njNRtpEE0E', 305295334)
-    collection = Premium.Kimp()
     # initialize bot functions
     bot.HandlerInitialize()
     bot.updater.start_polling()
     bot.updater.idle()
-
-    counter = 0
-    # main logic
-    #bot.SendTelegramMessage(collection.CalcPremiumPercentage(350.0,0.2793,1178.5))
-
-
 
 if __name__ == '__main__':
     main()
