d96c9dc5571fc3d52d135ee3ceb5dd6ae86cec59
C4NU <canu1832@gmail.com>
2021-09-23 16:03:00 +0900

    [FIXED] 안 쓰는 패키지 삭제.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import telegram_module as MessageModule
-import premium_module as PremiumModule
 
 # 메인 함수
 def main():
-    # initialize class
+    # initialize bot class
     bot = MessageModule.TelegramBot(
     	'2039263005:AAF_KVYVpcBHvBEbRPvlwtsj_njNRtpEE0E', 305295334)
     # initialize bot functions
     bot.HandlerInitialize()
+    # bot start updates
     bot.updater.start_polling()
     bot.updater.idle()
 
