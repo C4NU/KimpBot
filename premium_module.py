4a6265b9872e183d4c5558673cb8d4e5f24bca95
C4NU <canu1832@gmail.com>
2021-09-23 16:04:05 +0900

    [ADDED] 야후 파이낸스에서 USD/KRW 환율 받아오는 함수 작성. 안 쓰는 딕셔너리 변수 삭제.

--- premium_module.py
+++ premium_module.py
@@ -1,4 +1,7 @@
 import math # 김프 소수점 2자리 계산용 math 패키지
+from datetime import datetime # 오늘의 USD/KRW 환율 받아오기 위한 time 패키지
+
+import yfinance as yfi # 야후 파이낸스 python wrapper 패키지
 import pyupbit # 업비트 python wrapper 패키지
 from binance import Client # 바이낸스 API python wrapper 패키지
 
@@ -7,30 +10,23 @@ class Kimp:
     def __init__(self):
         self.client = Client('NDtnzdI8zBKeE6Ffq0PRjyYWq81NH4nsbUMsB09d9py0JPdaCeBl7SR7DHkVWExo',
          'IdUxUQNRUbMGYO4JPjbWEfcx4ZlANFNZvPut13WsFHG5Haebja8hw0sLIgC75Ftm') # 바이낸스 client 변수
-        self.xrp = {'NAME': 'XRP', 'PRICE': 0, 'KIMP': 0}  # 리플 딕셔너리 변수
-        self.xlm = {'NAME': 'XLM', 'PRICE': 0, 'KIMP': 0}  # 스텔라 딕셔너리 변수
-        self.eos = {'NAME': 'EOS', 'PRICE': 0, 'KIMP': 0}  # 이오스 딕셔너리 변수
-        self.trx = {'NAME': 'TRX', 'PRICE': 0, 'KIMP': 0}  # 트론 딕셔너리 변수
-        self.dot = {'NAME': 'DOT', 'PRICE': 0, 'KIMP': 0}  # 폴카닷 딕셔너리 변수
     # 김프 퍼센티지 계산 함수
     def CalcPremiumPercentage(self, name):
-        return round(((self.GetUpbitPrice(name)/(self.GetBinancePrice(name)*1184.44))*100)-100, 2) # 1184.44는 테더 가격 (임시)
+        return round(((self.GetUpbitPrice(name)/(self.GetBinancePrice(name)*self.GetUSDPrice()))*100)-100, 2) # 1184.44는 테더 가격 (임시)
     # 업비트 가격 긁어오는 함수, name 인자값에 xrp, xlm, eos, trx, dot
     def GetUpbitPrice(self, name):
         return pyupbit.get_current_price("KRW-"+name) # type: float
     # 바이낸스 가격 긁어오는 함수, name 인자값에 xrp / xlm / eos / trx / dot
     def GetBinancePrice(self, name):
         return float(self.client.get_ticker(symbol=name+'USDT')['lastPrice']) # type: float
-    #def GetUSDTPrice(self):
-    #    return float(self.client.get_ticker(symbol='USDT')['lastPrice']) # type: float
-
+    def GetUSDPrice(self):
+        result = yfi.download(['USDKRW=X'],start=datetime.today(), end=datetime.today())['Close'][0]
+        #result = pyupbit.get_current_price("KRW-BTC") / pyupbit.get_current_price("USDT-BTC") 업비트의 테더 가격..
+        return round(result,2)
 
 # 프리미엄 모듈 테스트용 메인 함수
 def main():
     premium = Kimp()
-    result = premium.CalcPremiumPercentage(premium.GetUpbitPrice('XRP'), premium.GetBinancePrice('XRP'), 1184.44)
-
-    print(result)
 
 # 메인 함수 구역
 if __name__ == '__main__':
