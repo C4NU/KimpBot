b84a69d1e7aeb6357a9ab2332f0dc32e27551b7f
C4NU <canu1832@gmail.com>
2022-12-21 01:48:01 +0900

    [FIX] timezone error

--- premium_module.py
+++ premium_module.py
@@ -25,9 +25,7 @@ class Kimp:
         return float(self.client.get_ticker(symbol=name+'USDT')['lastPrice']) # type: float
     # 야후 파이낸스에서 USD/KRW 받아오는 함수, pandas series 값.
     def GetUSDPrice(self):
-        startTime = datetime.today().strftime("%Y-%m-%d")
-        endTime = datetime.today().strftime("%Y-%m-%d")
-        result = yfi.download(['KRW=X'],startTime,endTime)['Close'][0]
+        result = yfi.download(['KRW=X'], period="1d")['Close'][0]
         #result = pyupbit.get_current_price("KRW-BTC") / pyupbit.get_current_price("USDT-BTC") 업비트의 테더 가격..
         return round(result,2)
 
