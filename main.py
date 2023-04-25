e9351d92310279a6d437b4b6ba29edeae253ae5f
C4NU <canu1832@gmail.com>
2023-04-25 20:11:21 +0900

    [FIX] main py 문제...

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import telegram_module as MessageModule # 텔레그렘 봇 모듈
 # 메인 함수
 def main():
     #with open("keys.json") as f:
-    with open("keys_sample==.json") as f:
+    with open("keys_sample.json") as f:
             data = json.load(f)
     # initialize bot class
     bot = MessageModule.TelegramBot(
